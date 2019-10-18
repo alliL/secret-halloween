6007c23110a5e59083dd09ca1767b5f228f1f3b6
Allison <allison.lee124@gmail.com>
2019-10-18 16:55:48 -0700

    Updates

---
+++ precache-manifest.dd196b3204bc90eda0886a302b61c357.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b13e5b9aff403454804505707d598df7",
+    "url": "/secret-halloween/index.html"
+  },
+  {
+    "revision": "fa0135058888e53640f4",
+    "url": "/secret-halloween/static/css/main.d600868b.chunk.css"
+  },
+  {
+    "revision": "794ba796c9c5d198cec0",
+    "url": "/secret-halloween/static/js/2.ce29ca7a.chunk.js"
+  },
+  {
+    "revision": "fa0135058888e53640f4",
+    "url": "/secret-halloween/static/js/main.d16d9a99.chunk.js"
+  },
+  {
+    "revision": "1bc2b680d42a09c15e20",
+    "url": "/secret-halloween/static/js/runtime-main.3e10e108.js"
+  }
+]);
